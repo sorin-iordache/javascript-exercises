b815efb54dacecb89dbd070e6904820b3839d7c3
sorin-iordache <sorin.robert768@gmail.com>
2024-06-25 13:22:00 +0300

    Solved Assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementsToRemove) {
+  return array.filter((item) => !elementsToRemove.includes(item));
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
