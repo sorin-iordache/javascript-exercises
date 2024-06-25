5528b1c0884ee8d17a1694e527f63daf83fab088
sorin-iordache <sorin.robert768@gmail.com>
2024-06-25 13:33:42 +0300

    Solved Assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+function sumAll(start, end) {
+  if (
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  ) {
+    return "ERROR";
+  }
 
-};
+  let sum = 0;
+
+  const min = Math.min(start, end);
+  const max = Math.max(start, end);
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+module.exports = sumAll;
 
 // Do not edit below this line
 module.exports = sumAll;
+``;
